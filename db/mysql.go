71011e68b154741795c36304881959914ef061ea
Huỳnh Ngọc Quí <qh20812@gmail.com>
2026-06-02 15:49:46 +0700

    update

--- db/mysql.go
+++ db/mysql.go
@@ -38,6 +38,6 @@ func ConnectDb(env config.Env) (*sql.DB, error) {
 	return db, nil
 }
 
-func registerTLSConfig(serverName string) (string, error) {
+func registerTLSConfig() (string, error) {
 	return "", fmt.Errorf("tls registration removed")
 }
