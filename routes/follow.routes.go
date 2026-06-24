625294bd54a2a1de3266a6100a1a3a85a6b2a2be
QuyenMaiStudent <quyenmxg.124010124083@vtc.edu.vn>
2026-06-24 15:32:21 +0700

    middlewares follow

--- routes/follow.routes.go
+++ routes/follow.routes.go
@@ -12,6 +12,6 @@ func RegisterFollowRoutes(router *gin.Engine, followController *controllers.Foll
 	follow := router.Group("/api/follow")
 	{
 		follow.POST("/:userID", middlewares.AuthMiddleware(env), followController.FollowToggle)
-		follow.GET("/stats/:userID", followController.GetFollowStats)
+		follow.GET("/stats/:userID", middlewares.AuthMiddleware(env),followController.GetFollowStats)
 	}
 }
